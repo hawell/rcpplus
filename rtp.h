6d9cb927ff79aa16107f26a57db8872408a2ebca
arash kordi <arash.cordi@gmail.com>
2012-09-01 11:52:30 +0430

    add warning for rtp code

--- rtp.h
+++ rtp.h
@@ -5,6 +5,12 @@
  *      Author: arash
  */
 
+/*
+ * WARNING: this is a minimal implementation of rtp payload format defined in rfc 3984 for h264 and sould not be 
+ * considered as part of rcpplus.
+ * a practical application should use a full implenetation, this file is only for test purposes.
+*/
+
 #ifndef RTP_H_
 #define RTP_H_
 
